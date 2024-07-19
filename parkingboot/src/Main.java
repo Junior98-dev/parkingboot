918e2c13ed2000706572a4e80741ba6966bd7ca0
Junior98-dev <kounkouddorianjunior@gmail.com>
2024-07-20 01:46:10 +0200

    Initialisation

---
+++ parkingboot/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int length = 180;
+        int width = 120;
+        int surface = length * width;
+        System.out.println("La surface au sol de la voiture est de: "+ surface);
+    }
+}
